6fdd0a4581c7749710d907c65dcd2f5286dd9a26
나윤주 <bubblenana@bubblecon.io>
2022-01-28 14:59:58 +0900

    Pdf Viewer Custom_20220128

--- web/viewer.js
+++ web/viewer.js
@@ -9135,82 +9135,68 @@ function resizesize() {
     toggleButton.setAttribute("aria-expanded", "false");
     outerContainer.classList.add("sidebarMoving");
     //모바일 터치 zoom 반영
-    let pinchZoomEnabled = false;
-
+  let pinchZoomEnabled = false;
     function enablePinchZoom(pdfViewer) {
-      let startX = 0,
-        startY = 0;
-      let initialPinchDistance = 0;
-      let pinchScale = 1;
-      const viewer = document.getElementById("viewer");
-      const container = document.getElementById("viewerContainer");
-      const reset = () => {
-        startX = startY = initialPinchDistance = 0;
-        pinchScale = 1;
-      };
-      // Prevent native iOS page zoom
-      //document.addEventListener("touchmove", (e) => { if (e.scale !== 1) { e.preventDefault(); } }, { passive: false });
-      document.addEventListener("touchstart", (e) => {
-        if (e.touches.length > 1) {
-          startX = (e.touches[0].pageX + e.touches[1].pageX) / 2;
-          startY = (e.touches[0].pageY + e.touches[1].pageY) / 2;
-          initialPinchDistance = Math.hypot((e.touches[1].pageX - e.touches[0].pageX), (e.touches[1].pageY - e.touches[0].pageY));
-        } else {
-          initialPinchDistance = 0;
-        }
-      });
+        let startX = 0, startY = 0;
+        let initialPinchDistance = 0;        
+        let pinchScale = 1;    
+        const viewer = document.getElementById("viewer");
+        const container = document.getElementById("viewerContainer");
+        const reset = () => { startX = startY = initialPinchDistance = 0; pinchScale = 1; };
+        // Prevent native iOS page zoom
+        //document.addEventListener("touchmove", (e) => { if (e.scale !== 1) { e.preventDefault(); } }, { passive: false });
+        document.addEventListener("touchstart", (e) => {
+            if (e.touches.length > 1) {
+                startX = (e.touches[0].pageX + e.touches[1].pageX) / 2;
+                startY = (e.touches[0].pageY + e.touches[1].pageY) / 2;
+                initialPinchDistance = Math.hypot((e.touches[1].pageX - e.touches[0].pageX), (e.touches[1].pageY - e.touches[0].pageY));
+            } else {
+                initialPinchDistance = 0;
+            }
+        });
 
-      document.addEventListener("touchmove", (e) => {
-        if (initialPinchDistance <= 0 || e.touches.length < 2) {
-          return;
-        }
-        if (e.scale !== 1) {
-          e.preventDefault();
-        }
-        const pinchDistance = Math.hypot((e.touches[1].pageX - e.touches[0].pageX), (e.touches[1].pageY - e.touches[0].pageY));
-        const originX = startX + container.scrollLeft;
-        const originY = startY + container.scrollTop;
-        pinchScale = pinchDistance / initialPinchDistance;
-        viewer.style.transform = `scale(${pinchScale})`;
-        viewer.style.transformOrigin = `${originX}px ${originY}px`;
-        console.log(viewer, '나와라')
-      }, {
-        passive: false
-      });
+        document.addEventListener("touchmove", (e) => {
+            if (initialPinchDistance <= 0 || e.touches.length < 2) { return; }
+            if (e.scale !== 1) { e.preventDefault(); }
+            const pinchDistance = Math.hypot((e.touches[1].pageX - e.touches[0].pageX), (e.touches[1].pageY - e.touches[0].pageY));
+            const originX = startX + container.scrollLeft;
+            const originY = startY + container.scrollTop;
+            pinchScale = pinchDistance / initialPinchDistance;
+            viewer.style.transform = `scale(${pinchScale})`;
+            viewer.style.transformOrigin = `${originX}px ${originY}px`;
+            console.log(viewer,'나와라')
+        }, { passive: false });
 
-      document.addEventListener("touchend", (e) => {
-        if (initialPinchDistance <= 0) {
-          return;
-        }
-        viewer.style.transform = `none`;
-        viewer.style.transformOrigin = `unset`;
-        PDFViewerApplication.pdfViewer.currentScale *= pinchScale;
-        const rect = container.getBoundingClientRect();
-        const dx = startX - rect.left;
-        const dy = startY - rect.top;
-        container.scrollLeft += dx * (pinchScale - 1);
-        container.scrollTop += dy * (pinchScale - 1);
-        reset();
-      });
+        document.addEventListener("touchend", (e) => {
+            if (initialPinchDistance <= 0) { return; }
+            viewer.style.transform = `none`;
+            viewer.style.transformOrigin = `unset`;
+            PDFViewerApplication.pdfViewer.currentScale *= pinchScale;
+            const rect = container.getBoundingClientRect();
+            const dx = startX - rect.left;
+            const dy = startY - rect.top;
+            container.scrollLeft += dx * (pinchScale - 1);
+            container.scrollTop += dy * (pinchScale - 1);
+            reset();
+        });
     }
     document.addEventListener('webviewerloaded', () => {
-      if (!pinchZoomEnabled) {
-        pinchZoomEnabled = true;
-        enablePinchZoom();
-      }
+        if (!pinchZoomEnabled) {
+            pinchZoomEnabled = true;
+            enablePinchZoom();
+        }
     });
 
     // evt.mode == 3;
-    }
-    else if (isDesktop) {
-      console.log('Desktop');
-      toggleButton.classList.add("toggled");
-      outerContainer.classList.add("sidebarOpen");
-      toggleButton.setAttribute("aria-expanded", "true");
-      outerContainer.classList.remove("sidebarMoving");
-    } else {
-      console.log('Not found');
-    }
+  } else if (isDesktop) {
+    console.log('Desktop');
+    toggleButton.classList.add("toggled");
+    outerContainer.classList.add("sidebarOpen");
+    toggleButton.setAttribute("aria-expanded", "true");
+    outerContainer.classList.remove("sidebarMoving");
+  } else {
+    console.log('Not found');
+  }
 
   //패드형부터는 위아래로 스크롤시 작동
 }
