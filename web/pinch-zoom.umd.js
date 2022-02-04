9bd4d8c13e52353cc85c3f7ae8167e42bb355382
나윤주 <bubblenana@bubblecon.io>
2022-02-04 14:58:07 +0900

    pdf js

--- web/pinch-zoom.umd.js
+++ web/pinch-zoom.umd.js
@@ -659,9 +659,9 @@
                 this.container.style.overflow = 'scroll';
                 this.container.style.position = 'relative';
 
-                this.container.style.display = 'flex';
-                this.container.style.alignItems="center";
-                this.container.style.flexDirection="column";
+                // this.container.style.display = 'flex';
+                // this.container.style.alignItems="center";
+                // this.container.style.flexDirection="column";
 
                 this.el.style.webkitTransformOrigin = '0% 0%';
                 this.el.style.mozTransformOrigin = '0% 0%';
