cb8fd15e5b02a3b580422e468bd6f2c69669be7b
나윤주 <bubblenana@bubblecon.io>
2022-02-16 17:56:02 +0900

    pdf change test

--- web/viewer_new.js
+++ web/viewer_new.js
@@ -72,7 +72,7 @@ const defaultOptions = {
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE
   },
   defaultUrl: {
-    value: "compressed.tracemonkey-pldi-09.pdf",
+    value: "KBB-300011-3011036582004_SIMSA.pdf",
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
